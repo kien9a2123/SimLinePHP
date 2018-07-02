e5423cbdc680e3f9c71025d0ef7e4d1e4423ddf3
Cybertk <32348809+tolga000@users.noreply.github.com>
2018-07-02 06:36:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = '722f84ae-dae4-4d1b-a738-3a8b90ccb957'; //API SimSimi
+$key = '93f2d5d9-cacb-471d-a133-b02d82e2a500'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=tr&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
