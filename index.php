19388f99ed3397b4b028f2c799054c082d6f52f1
Cybertk <32348809+tolga000@users.noreply.github.com>
2018-07-02 05:16:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if($message['type']=='sticker')
 							'messages' => array(
 								array(
 										'type' => 'text',									
-										'text' => 'Sticker palakan aja di pamerin.'										
+										'text' => ':)'										
 									
 									)
 							)
